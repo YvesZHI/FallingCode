e76d12ae6ebe0aa72a7e0dc305094a7aba72328e
Yuhui ZHI <yuhuizhi@MBP-de-Yuhui.fritz.box>
2016-11-15 04:42:48 +0100

    add comment

--- Data.h
+++ Data.h
@@ -66,7 +66,7 @@ public:
 					{
 						color = color_head;
 					}
-					else if (len - delta == 1)
+					else if (len - delta == 1) // this should be "1.0*delta/pos<0.7" but this calculation is too heavy
 					{
 						color = color_tail;
 					}
